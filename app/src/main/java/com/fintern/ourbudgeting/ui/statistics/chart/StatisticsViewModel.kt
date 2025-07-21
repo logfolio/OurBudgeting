a90654fc2332c22c6a5647cc6abf86b6b7d85815
Meezzi <alswl-3016@naver.com>
2025-07-21 19:35:46 +0900

    feat: StatisticsViewModel 추가 - 통계 화면 상태 관리

---
+++ app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsViewModel.kt
@@ -0,0 +1,11 @@
+package com.fintern.ourbudgeting.ui.statistics.chart
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class StatisticsViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(StatisticsUiState())
+    val uiState: StateFlow<StatisticsUiState> = _uiState.asStateFlow()
+}
