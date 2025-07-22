ce9a4ecd9abd22f9863c711c021e73ff174721ae
Meezzi <alswl-3016@naver.com>
2025-07-22 22:02:54 +0900

    feat: fetchMonthlyCategoryTotals - 시작/끝 날짜 계산 로직 구현

--- app/src/main/java/com/fintern/ourbudgeting/data/repository/StatisticsRepository.kt
+++ app/src/main/java/com/fintern/ourbudgeting/data/repository/StatisticsRepository.kt
@@ -1,6 +1,8 @@
 package com.fintern.ourbudgeting.data.repository
 
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Calendar
 
 class StatisticsRepository(
     private val firestore: FirebaseFirestore
@@ -12,7 +14,29 @@ class StatisticsRepository(
         uid: String,
         householdId: String
     ): Map<String, Double> {
+        val startTimestamp = getStartTimestamp(year, month)
+        val endTimestamp = getEndTimestamp(year, month)
 
-        return mapOf();
+        return mapOf()
+    }
+
+    private fun getStartTimestamp(year: Int, month: Int): Timestamp {
+        val cal = Calendar.getInstance().apply {
+            clear()
+            set(Calendar.YEAR, year)
+            set(Calendar.MONTH, month - 1)
+            set(Calendar.DAY_OF_MONTH, 1)
+        }
+        return Timestamp(cal.time)
+    }
+
+    private fun getEndTimestamp(year: Int, month: Int): Timestamp {
+        val cal = Calendar.getInstance().apply {
+            clear()
+            set(Calendar.YEAR, year)
+            set(Calendar.MONTH, month)
+            set(Calendar.DAY_OF_MONTH, 1)
+        }
+        return Timestamp(cal.time)
     }
 }
