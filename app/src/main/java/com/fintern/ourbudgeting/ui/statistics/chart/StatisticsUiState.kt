30854d33bb9dc70daff8d1db5ec57335ed1f4715
Meezzi <alswl-3016@naver.com>
2025-07-23 17:07:26 +0900

    feat: StatisticsUiState 통계 UI 상태에 ChartData, isLoading, errorMessage 추가

--- app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsUiState.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsUiState.kt
@@ -1,6 +1,11 @@
 package com.fintern.ourbudgeting.ui.statistics.chart
 
+import com.fintern.ourbudgeting.data.chart.PieEntry
+
 data class StatisticsUiState(
     val currentYear: Int = 2025,
     val currentMonth: Int = 1,
+    val chartData: List<PieEntry> = emptyList(),
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
 )
