2e86bc0e89932bea4188e8f51aca5c8dc527d962
Meezzi <alswl-3016@naver.com>
2025-07-23 20:10:15 +0900

    feat: StatisticsViewModel에 hilt로 repository 의존성 주입

--- app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsViewModel.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsViewModel.kt
@@ -1,11 +1,17 @@
 package com.fintern.ourbudgeting.ui.statistics.chart
 
 import androidx.lifecycle.ViewModel
+import com.fintern.ourbudgeting.data.repository.StatisticsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
-class StatisticsViewModel : ViewModel() {
+@HiltViewModel
+class StatisticsViewModel @Inject constructor(
+    private val repository: StatisticsRepository
+) : ViewModel() {
     private val _uiState = MutableStateFlow(StatisticsUiState())
     val uiState: StateFlow<StatisticsUiState> = _uiState.asStateFlow()
 }
