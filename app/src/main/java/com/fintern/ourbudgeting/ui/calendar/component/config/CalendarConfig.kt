4e9da1f0f6aa70490267e1e78eaab6992d0c7fce
mandol96 <chocan0202@gmail.com>
2025-07-23 17:40:54 +0900

    feat: 요일 표시 config 생성

--- app/src/main/java/com/fintern/ourbudgeting/ui/calendar/component/config/CalendarConfig.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/calendar/component/config/CalendarConfig.kt
@@ -1,5 +1,6 @@
 package com.fintern.ourbudgeting.ui.calendar.component.config
 
 data class CalendarConfig(
-    val calendarHeaderConfig: CalendarHeaderConfig = CalendarHeaderConfig.default()
+    val calendarHeaderConfig: CalendarHeaderConfig = CalendarHeaderConfig.default(),
+    val calendarDayLabelConfig: CalendarDayLabelConfig = CalendarDayLabelConfig.default(),
 )
