7b293ce72c4a5be06c98520be8236a690f862463
Meezzi <alswl-3016@naver.com>
2025-07-24 20:01:42 +0900

    feat: UIState의 isLoading이 true라면 CircularProgressIndicator 표시

--- app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsScreen.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsScreen.kt
@@ -1,7 +1,9 @@
 package com.fintern.ourbudgeting.ui.statistics.chart
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -11,10 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.fintern.ourbudgeting.R
 import com.fintern.ourbudgeting.ui.statistics.chart.components.MonthSelector
@@ -63,10 +67,15 @@ fun StatisticsScreen(
             )
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
         ) {
-            // TODO: 통계 화면 본문 작성
+            when {
+                uiState.isLoading -> CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
         }
     }
 }
