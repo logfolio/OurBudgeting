93741897b46507cc7dc417c38107c2315bbeaa0b
Meezzi <alswl-3016@naver.com>
2025-07-25 08:50:30 +0900

    feat: StatisticsUIState에 수입/지출 유형 추가

--- app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsUiState.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsUiState.kt
@@ -1,11 +1,13 @@
 package com.fintern.ourbudgeting.ui.statistics.chart
 
 import com.fintern.ourbudgeting.data.chart.PieEntry
+import com.fintern.ourbudgeting.ui.statistics.chart.model.TransactionType
 
 data class StatisticsUiState(
     val currentYear: Int = 2025,
     val currentMonth: Int = 1,
     val chartData: List<PieEntry> = emptyList(),
     val isLoading: Boolean = false,
-    val errorMessage: String? = null
+    val errorMessage: String? = null,
+    val currentType: TransactionType = TransactionType.EXPENSE,
 )
