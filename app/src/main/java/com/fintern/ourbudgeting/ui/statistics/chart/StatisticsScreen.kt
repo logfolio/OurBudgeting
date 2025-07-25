3b3a25fd0adcc3193254836813e395df70a033d1
Meezzi <alswl-3016@naver.com>
2025-07-25 09:21:51 +0900

    feat: StatisticsScreen의 Tab 추가

--- app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsScreen.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsScreen.kt
@@ -8,22 +8,29 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.fintern.ourbudgeting.R
 import com.fintern.ourbudgeting.ui.statistics.chart.components.MonthSelector
 import com.fintern.ourbudgeting.ui.statistics.chart.components.PieChart
+import com.fintern.ourbudgeting.ui.statistics.chart.components.StatisticsTab
 import com.fintern.ourbudgeting.ui.theme.OurBudgetingTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,6 +41,8 @@ fun StatisticsScreen(
     statisticsViewModel: StatisticsViewModel = hiltViewModel()
 ) {
     val uiState by statisticsViewModel.uiState.collectAsState()
+    val startDestination = StatisticsTab.EXPENSE
+    var selectedTabIndex by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
 
     Scaffold(
         topBar = {
@@ -64,6 +73,31 @@ fun StatisticsScreen(
             )
         }
     ) { innerPadding ->
+        PrimaryTabRow(
+            selectedTabIndex = selectedTabIndex,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            StatisticsTab.entries.forEachIndexed { index, destination ->
+                Tab(
+                    selected = selectedTabIndex == index,
+                    onClick = {
+                        selectedTabIndex = index
+                        statisticsViewModel.updateTypeAndFetchData(
+                            type = destination.type,
+                            uid = uid,
+                            householdId = householdId
+                        )
+                    },
+                    text = {
+                        Text(
+                            text = destination.label,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                )
+            }
+        }
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
