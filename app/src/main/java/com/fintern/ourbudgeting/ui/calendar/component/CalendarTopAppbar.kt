426db394406cc606c3dbf0cc1dd6805710a57dfe
mandol96 <chocan0202@gmail.com>
2025-07-27 20:38:11 +0900

    feat: topappbar title 센터 배치

--- app/src/main/java/com/fintern/ourbudgeting/ui/calendar/component/CalendarTopAppbar.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/calendar/component/CalendarTopAppbar.kt
@@ -1,5 +1,7 @@
 package com.fintern.ourbudgeting.ui.calendar.component
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -10,6 +12,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -20,10 +24,16 @@ fun CalendarTopAppbar(
 ) {
     TopAppBar(
         title = {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleLarge
-            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
         },
         actions = {
             IconButton(onClick = onNotificationClick) {
