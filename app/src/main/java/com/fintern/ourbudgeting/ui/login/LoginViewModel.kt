4d9937abf33d74041024c0eec59fa58e86968c42
Meezzi <alswl-3016@naver.com>
2025-07-28 17:19:53 +0900

    feat: Google Login을 처리하고 상태를 업데이트하는 LoginViewModel 추가
    Co-authored-by: mandol96 <chocan0202@gmail.com>
    Co-authored-by: 0508gyhun <0508gyhun@naver.com>

---
+++ app/src/main/java/com/fintern/ourbudgeting/ui/login/LoginViewModel.kt
@@ -0,0 +1,35 @@
+package com.fintern.ourbudgeting.ui.login
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fintern.ourbudgeting.data.repository.GoogleAuthRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val authRepository: GoogleAuthRepository
+) : ViewModel() {
+
+    private val currentUser = authRepository.getCurrentUser()
+
+    private val _uiState = MutableStateFlow(
+        LoginUiState(currentUser = currentUser)
+    )
+    val uiState = _uiState.asStateFlow()
+
+    fun signInWithGoogle(context: Context) {
+        viewModelScope.launch {
+            try {
+                val user = authRepository.signInWithGoogle(context)
+                _uiState.value = _uiState.value.copy(currentUser = user)
+            } catch (e: Exception) {
+                // TODO: 에러 처리
+            }
+        }
+    }
+}
