cde7055ceb21c6b8da2b63cbf168f2cbc7386b60
Meezzi <alswl-3016@naver.com>
2025-07-29 11:21:34 +0900

    feat: uid, nickname을 DataStore에서 읽어오는 readUser 구현
    Co-authored-by: mandol96 <chocan0202@gmail.com>
    Co-authored-by: 0508gyhun <0508gyhun@naver.com>

--- app/src/main/java/com/fintern/ourbudgeting/data/repository/UserPreferencesRepository.kt
+++ app/src/main/java/com/fintern/ourbudgeting/data/repository/UserPreferencesRepository.kt
@@ -1,9 +1,14 @@
 package com.fintern.ourbudgeting.data.repository
 
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.IOException
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class UserPreferencesRepository @Inject constructor(
@@ -19,6 +24,21 @@ class UserPreferencesRepository @Inject constructor(
             preferences[PreferencesKeys.UID] = uid
             preferences[PreferencesKeys.NICKNAME] = nickname
         }
+    }
+
+    fun readUser(): Flow<UserPreferences> {
+        return dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }.map { preferences ->
+                val uid = preferences[PreferencesKeys.UID] ?: ""
+                val nickname = preferences[PreferencesKeys.NICKNAME] ?: ""
+                UserPreferences(uid, nickname)
+            }
     }
 }
 
