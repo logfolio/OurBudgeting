8676eb8ecbadc652e775698f737ca04fe4861dc1
mandol96 <chocan0202@gmail.com>
2025-07-30 18:42:58 +0900

    refactor: Preview 삭제

--- app/src/main/java/com/fintern/ourbudgeting/ui/calendar/CalendarScreen.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/calendar/CalendarScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.fintern.ourbudgeting.R
 import com.fintern.ourbudgeting.data.calendar.CategoryDefinition
@@ -38,7 +37,6 @@ import com.google.firebase.Timestamp
 import java.time.DayOfWeek
 import java.time.LocalDate
 
-@Preview
 @Composable
 fun CalendarScreen() {
 
