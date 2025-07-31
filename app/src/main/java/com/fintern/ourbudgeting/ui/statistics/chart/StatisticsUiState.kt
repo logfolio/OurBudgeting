90a3c45b26a3238010132d908660ab757e07ee02
Meezzi <alswl-3016@naver.com>
2025-07-31 10:06:57 +0900

    feat: StatisticsError sealed class 생성

--- app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsUiState.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/statistics/chart/StatisticsUiState.kt
@@ -1,5 +1,7 @@
 package com.fintern.ourbudgeting.ui.statistics.chart
 
+import androidx.annotation.StringRes
+import com.fintern.ourbudgeting.R
 import com.fintern.ourbudgeting.data.chart.PieEntry
 import com.fintern.ourbudgeting.ui.common.model.TransactionType
 
@@ -8,6 +10,12 @@ data class StatisticsUiState(
     val currentMonth: Int = 1,
     val chartData: List<PieEntry> = emptyList(),
     val isLoading: Boolean = false,
-    val errorMessage: String? = null,
+    val error: StatisticsError? = null,
     val currentType: TransactionType = TransactionType.EXPENSE,
 )
+
+sealed class StatisticsError(@StringRes val messageResId: Int) {
+    object NetworkError : StatisticsError(R.string.error_network)
+    object FirestoreError : StatisticsError(R.string.error_firestore)
+    object UnknownError : StatisticsError(R.string.error_unknown)
+}
