830437119254d0bbd8d9f84d32814506865a74d3
Meezzi <alswl-3016@naver.com>
2025-07-31 16:27:36 +0900

    refactor: TransactionToggle에 modifier 전달

--- app/src/main/java/com/fintern/ourbudgeting/ui/save/componenet/TransactionToggle.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/save/componenet/TransactionToggle.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -20,14 +19,14 @@ import com.fintern.ourbudgeting.ui.common.model.TransactionType
 
 @Composable
 fun TransactionToggle(
+    modifier: Modifier = Modifier,
     transactionType: TransactionType,
     onTransactionTypeChange: (TransactionType) -> Unit,
 ) {
 
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
+        modifier = modifier
+            .fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         ToggleButton(
