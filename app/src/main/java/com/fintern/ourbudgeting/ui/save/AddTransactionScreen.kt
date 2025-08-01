59e5e98f951caf12e99c63e8201d5827360d22e1
Meezzi <alswl-3016@naver.com>
2025-08-01 13:41:43 +0900

    feat: 자산 선택 구현

--- app/src/main/java/com/fintern/ourbudgeting/ui/save/AddTransactionScreen.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/save/AddTransactionScreen.kt
@@ -5,17 +5,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.fintern.ourbudgeting.R
+import com.fintern.ourbudgeting.data.model.ExpenseCategoryType
+import com.fintern.ourbudgeting.data.model.IncomeCategoryType
 import com.fintern.ourbudgeting.ui.common.model.TransactionType
 import com.fintern.ourbudgeting.ui.save.componenet.DatePickerField
+import com.fintern.ourbudgeting.ui.save.componenet.DropDownField
 import com.fintern.ourbudgeting.ui.save.componenet.TransactionToggle
 import com.fintern.ourbudgeting.ui.theme.OurBudgetingTheme
 
@@ -24,9 +26,7 @@ fun AddTransactionScreen(
     initialTransactionType: TransactionType,
     modifier: Modifier = Modifier,
 ) {
-    var uiState by remember {
-        mutableStateOf(AddTransactionUiState(transactionType = initialTransactionType))
-    }
+    val uiState by viewModel.uiState.collectAsState()
 
     Scaffold { innerPadding ->
         Column(
@@ -48,6 +48,16 @@ fun AddTransactionScreen(
                     uiState = uiState.copy(selectedDate = transactionDate)
                 },
                 modifier = modifier
+            )
+
+            // 자산 선택
+            DropDownField(
+                label = stringResource(R.string.asset),
+                options = listOf("은행 계좌", "현금", "카드"),
+                // TODO: Firestore에서 데이터 불러와서 표시
+                onOptionSelected = { selected ->
+                    viewModel.setSelectedAsset(selected)
+                },
             )
         }
     }
---
+++ app/src/main/java/com/fintern/ourbudgeting/ui/save/AddTransactionViewModel.kt
@@ -0,0 +1,17 @@
+package com.fintern.ourbudgeting.ui.save
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+@HiltViewModel
+class AddTransactionViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(AddTransactionUiState())
+    val uiState: StateFlow<AddTransactionUiState> = _uiState
+
+    fun setSelectedAsset(asset: String) {
+        _uiState.update { it.copy(selectedAsset = asset) }
+    }
+}
