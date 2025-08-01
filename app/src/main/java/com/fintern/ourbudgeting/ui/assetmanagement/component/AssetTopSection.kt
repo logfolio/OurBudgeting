b09f8446859a0c4061bb957555bd715b8b8d9579
mandol96 <chocan0202@gmail.com>
2025-08-01 13:02:03 +0900

    fix: 충돌 해결

---
+++ app/src/main/java/com/fintern/ourbudgeting/ui/assetmanagement/component/AssetTopSection.kt
@@ -0,0 +1,64 @@
+package com.fintern.ourbudgeting.ui.assetmanagement.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fintern.ourbudgeting.R
+
+@Composable
+fun AssetTopSection(
+    modifier: Modifier = Modifier,
+    asset: Long,
+    debt: Long,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(Color.White),
+        horizontalArrangement = Arrangement.Absolute.SpaceAround
+    ) {
+        Column(
+            modifier = Modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(stringResource(R.string.asset))
+            Text(stringResource(R.string.amount_won,asset), color = Color.Blue)
+        }
+        Column(
+            modifier = Modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(stringResource(R.string.debt))
+            Text(stringResource(R.string.amount_won,debt), color = Red)
+        }
+        Column(
+            modifier = Modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(stringResource(R.string.total))
+            Text(stringResource(R.string.amount_won,asset-debt))
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun TopSectionPreview() {
+    MaterialTheme {
+        AssetTopSection(asset = 30000, debt = 50000)
+    }
+}
---
+++ app/src/main/java/com/fintern/ourbudgeting/ui/assetmanagement/component/StatisticAssetTopAppBar.kt
@@ -0,0 +1,82 @@
+package com.fintern.ourbudgeting.ui.assetmanagement.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fintern.ourbudgeting.R
+import java.util.Calendar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StatisticsAssetTopAppBar(
+    modifier: Modifier = Modifier,
+    onActionClick: () -> Unit = {}
+) {
+    val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+    var year by rememberSaveable { mutableStateOf(currentYear) }
+    TopAppBar(
+        modifier = modifier,
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.White,
+            titleContentColor = Color.Black,
+        ),
+        actions = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { year-- }) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_navigate_before),
+                        contentDescription = stringResource(R.string.previous_year)
+                    )
+                }
+                Text(
+                    text = "$year",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(horizontal = 8.dp)
+                )
+                IconButton(onClick = { year++ }) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_navigate_next),
+                        contentDescription = stringResource(R.string.next_year)
+                    )
+                }
+            }
+        },
+        title = { Text(stringResource(R.string.total_statistic)) },
+        navigationIcon = {
+            IconButton(onClick = onActionClick) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_arrowback),
+                    contentDescription = stringResource(R.string.graph)
+                )
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StatisticAssetTopAppBarPreview() {
+    MaterialTheme {
+        StatisticsAssetTopAppBar()
+    }
+}
