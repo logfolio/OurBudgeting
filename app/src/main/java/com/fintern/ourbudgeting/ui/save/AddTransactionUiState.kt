093ffdab4d26f98f6cdf2fabcded5a6e44d733f0
Meezzi <alswl-3016@naver.com>
2025-08-01 13:38:04 +0900

    feat: AddTransactionUiState에 selectedAsset, selectedCategory 필드 추가

--- app/src/main/java/com/fintern/ourbudgeting/ui/save/AddTransactionUiState.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/save/AddTransactionUiState.kt
@@ -4,5 +4,7 @@ import com.fintern.ourbudgeting.ui.common.model.TransactionType
 
 data class AddTransactionUiState(
     val transactionType: TransactionType = TransactionType.EXPENSE,
-    val selectedDate: Long? = null
+    val selectedDate: Long? = null,
+    val selectedAsset: String? = null,
+    val selectedCategory: String? = null,
 )
