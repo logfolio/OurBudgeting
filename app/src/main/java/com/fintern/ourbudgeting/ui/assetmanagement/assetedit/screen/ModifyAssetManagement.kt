01141610869e9602167bb0d2bf60571d00fad177
0508gyhun <0508gyhun@naver.com>
2025-08-01 10:23:11 +0900

    refactor: 파일 이름 변경

--- app/src/main/java/com/fintern/ourbudgeting/ui/assetmanagement/assetedit/screen/ModifyAssetManagement.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/assetmanagement/assetedit/screen/ModifyAssetManagement.kt
@@ -1,4 +1,4 @@
-package com.fintern.ourbudgeting.ui.assetmanagement.screen
+package com.fintern.ourbudgeting.ui.assetmanagement.assetedit.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -8,17 +8,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.fintern.ourbudgeting.R
-import com.fintern.ourbudgeting.ui.assetmanagement.component.AssetEditBody
-import com.fintern.ourbudgeting.ui.assetmanagement.component.AssetEditTopAppBar
-import com.fintern.ourbudgeting.ui.assetmanagement.component.AssetTitle
-import com.fintern.ourbudgeting.ui.assetmanagement.component.AssetTopSection
+import com.fintern.ourbudgeting.ui.assetmanagement.common.component.AssetTitle
+import com.fintern.ourbudgeting.ui.assetmanagement.common.component.AssetTopSection
+import com.fintern.ourbudgeting.ui.assetmanagement.assetedit.component.AssetEditBody
+import com.fintern.ourbudgeting.ui.assetmanagement.assetedit.component.AssetEditTopAppBar
 import com.fintern.ourbudgeting.ui.theme.OurBudgetingTheme
 
 @Composable
-fun AssetEditManagement(
+fun ModifyAssetManagementScreen(
     modifier: Modifier = Modifier,
     asset: Long,
     debt: Long
@@ -34,10 +32,10 @@ fun AssetEditManagement(
                 .padding(paddingValue)
         ) {
             AssetTopSection(asset = asset, debt = debt)
-            AssetTitle(name = stringResource(R.string.cash), amount = 1000)
-            AssetEditBody(name = stringResource(R.string.cash), amount = 1000)
-            AssetTitle(name = stringResource(R.string.card), amount = 2000)
-            AssetEditBody(name = stringResource(R.string.card), amount = 1000)
+            AssetTitle(name = "현금", amount = 1000)
+            AssetEditBody(name = "현금", amount = 1000)
+            AssetTitle(name = "카드", amount = 2000)
+            AssetEditBody(name = "카드", amount = 1000)
         }
     }
 }
@@ -46,6 +44,6 @@ fun AssetEditManagement(
 @Composable
 fun ModifyAssetManagementScreenPreview() {
     OurBudgetingTheme {
-        AssetEditManagement(asset = 300, debt = 2000)
+        ModifyAssetManagementScreen(asset = 300, debt = 2000)
     }
 }
