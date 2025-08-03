45175902cb1fd30bb19b6ead6bbc9f6e4ff3f544
Meezzi <alswl-3016@naver.com>
2025-08-03 21:55:31 +0900

    feat: Firebase Storage를 의존성 주입할 수 있도록 Hilt 모듈 생성

--- app/src/main/java/com/fintern/ourbudgeting/di/FirebaseModule.kt
+++ app/src/main/java/com/fintern/ourbudgeting/di/FirebaseModule.kt
@@ -2,6 +2,7 @@ package com.fintern.ourbudgeting.di
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,6 +16,10 @@ object FirebaseModule {
     @Provides
     @Singleton
     fun provideFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideFirebaseStorage(): FirebaseStorage = FirebaseStorage.getInstance()
 
     @Provides
     @Singleton
