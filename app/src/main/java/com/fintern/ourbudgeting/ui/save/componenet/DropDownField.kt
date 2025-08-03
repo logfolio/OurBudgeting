8a3e3515fae4ae3fbea8ce22d09ce1975d1ad03a
Meezzi <alswl-3016@naver.com>
2025-08-03 10:46:19 +0900

    refactor: DropdownMenu를 ExposedDropdownMenu로 변경

--- app/src/main/java/com/fintern/ourbudgeting/ui/save/componenet/DropDownField.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/save/componenet/DropDownField.kt
@@ -1,15 +1,10 @@
 package com.fintern.ourbudgeting.ui.save.componenet
 
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.waitForUpOrCancellation
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,15 +12,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.DpOffset
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.PopupProperties
-import com.fintern.ourbudgeting.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DropDownField(
     label: String,
@@ -35,41 +23,25 @@ fun DropDownField(
     var expanded by remember { mutableStateOf(false) }
     var selectedOption by remember { mutableStateOf("") }
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded }
     ) {
-
         CommonOutlinedTextField(
             value = selectedOption,
             onValueChange = {},
-            label = label,
             readOnly = true,
+            label = label,
             trailingIcon = {
-                Icon(
-                    imageVector = Icons.Default.ArrowDropDown,
-                    contentDescription = stringResource(R.string.date_picker),
-                    tint = Color(0xFF964BFF)
-                )
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
             },
-            modifier = Modifier
-                .fillMaxWidth()
-                .pointerInput(expanded) {
-                    awaitEachGesture {
-                        awaitFirstDown(pass = PointerEventPass.Initial)
-                        val upEvent = waitForUpOrCancellation(pass = PointerEventPass.Initial)
-                        if (upEvent != null) {
-                            expanded = true
-                        }
-                    }
-                },
+            modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryEditable, enabled = true)
+
         )
-        DropdownMenu(
+
+        ExposedDropdownMenu(
             expanded = expanded,
-            onDismissRequest = { expanded = false },
-            modifier = Modifier.fillMaxWidth(),
-            offset = DpOffset(0.dp, 0.dp),
-            properties = PopupProperties(focusable = true)
+            onDismissRequest = { expanded = false }
         ) {
             options.forEach { selectionOption ->
                 DropdownMenuItem(
