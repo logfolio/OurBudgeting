9a75db4c800ab2f09625132704f04c9115e1e479
0508gyhun <0508gyhun@naver.com>
2025-08-04 10:51:55 +0900

    refactor: 자산유형추가 버튼 매개변수 구현 수정

--- app/src/main/java/com/fintern/ourbudgeting/ui/assetmanagement/component/AddAssetButton.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/assetmanagement/component/AddAssetButton.kt
@@ -14,12 +14,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun AddAssetButton(title: String, modifier: Modifier = Modifier) {
+fun AddAssetButton(
+    title: String,
+    modifier: Modifier = Modifier,
+    onClick: ()-> Unit
+) {
     Button(
         modifier = modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp, vertical = 6.dp),
-        onClick = {},
+        onClick = onClick,
         colors = ButtonDefaults.buttonColors(
             containerColor = Color(0xFF964BFF),
             contentColor = Color.White
@@ -33,5 +37,5 @@ fun AddAssetButton(title: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun AddAssetButtonPreview() {
-    MaterialTheme { AddAssetButton("추가 하기") }
+    MaterialTheme { AddAssetButton(title = "추가 하기", onClick = {}) }
 }
