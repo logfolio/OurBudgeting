25d7ff73055d39da512b85cd96ce1518bbba15a7
0508gyhun <0508gyhun@naver.com>
2025-08-04 10:50:18 +0900

    feat: 자산유형추가에 필요한 UiState 구현

---
+++ app/src/main/java/com/fintern/ourbudgeting/ui/assetmanagement/AssetTypeUiState.kt
@@ -0,0 +1,7 @@
+package com.fintern.ourbudgeting.ui.assetmanagement
+
+data class AssetTypeUiState(
+    val input: String = "",
+    val message: String = "",
+    val isLoading: Boolean = false
+)
