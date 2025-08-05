49e7a2c57ee9954e137ffb5eeefa7d1e67629d11
mandol96 <chocan0202@gmail.com>
2025-08-05 12:18:47 +0900

    feat: 개인 가계부 관리 ui 생성

---
+++ app/src/main/java/com/fintern/ourbudgeting/ui/household/PersonalHouseholdManagementScreen.kt
@@ -0,0 +1,112 @@
+package com.fintern.ourbudgeting.ui.household
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.fintern.ourbudgeting.R
+import com.fintern.ourbudgeting.ui.household.component.HouseHoldTopAppbar
+import com.fintern.ourbudgeting.ui.save.componenet.CommonOutlinedTextField
+
+@Composable
+fun PersonalHouseholdManagementScreen(
+    modifier: Modifier = Modifier,
+) {
+
+    var textFieldValue by remember { mutableStateOf("ㅁㅁ가계부") }
+    val isSaveEnabled = textFieldValue.isNotEmpty()
+
+    Scaffold(
+        modifier = Modifier.background(Color.White),
+        topBar = { HouseHoldTopAppbar() },
+        containerColor = Color.White,
+        contentWindowInsets = WindowInsets(0.dp)
+    ) { innerPadding ->
+        Column(
+            modifier = modifier
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth(),
+        ) {
+            Text(
+                text = stringResource(R.string.label_personal_household),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            CommonOutlinedTextField(
+                value = textFieldValue,
+                onValueChange = { newValue ->
+                    textFieldValue = newValue
+                },
+                label = "",
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+                    // TODO: 수정
+                },
+                enabled = isSaveEnabled,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(48.dp),
+                shape = RoundedCornerShape(4.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF964BFF)
+                )
+            ) {
+                Text(
+                    text = stringResource(R.string.label_edit),
+                    color = Color.White
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    // TODO: 초기화
+                },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(48.dp),
+                shape = RoundedCornerShape(4.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red
+                )
+            ) {
+                Text(
+                    text = stringResource(R.string.label_reset),
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
