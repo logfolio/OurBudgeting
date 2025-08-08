d0b4f681d505e7c523af0e71104d21b2c71047bf
Meezzi <alswl-3016@naver.com>
2025-08-08 17:32:32 +0900

    feat: OCR parsing하는 ReceiptOcrConfig 생성

--- app/src/main/java/com/fintern/ourbudgeting/util/ReceiptOcrParser.kt
+++ app/src/main/java/com/fintern/ourbudgeting/util/ReceiptOcrParser.kt
@@ -4,3 +4,9 @@ data class ReceiptParsedResult(
     val amount: String? = null,
     val storeName: String? = null,
 )
+
+object ReceiptOcrConfig {
+    val AMOUNT_REGEX = Regex("""(\d{1,3}(,\d{3})+|\d+)([원₩])?""")
+    val AMOUNT_UNIT_SYMBOLS = listOf(",", "₩", "원")
+    val STORE_NAME_KEYWORDS = listOf("마트", "점", "대표")
+}
