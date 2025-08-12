fdacb6692f2cbcee503e16cbe0ff5f0c5085632b
0508gyhun <0508gyhun@naver.com>
2025-08-12 16:01:08 +0900

    refactor: AssetEditScreen 상태관리를 위해 코드 수정

--- app/src/main/java/com/fintern/ourbudgeting/ui/assetmanagement/assetedition/AssetEditScreen.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/assetmanagement/assetedition/AssetEditScreen.kt
@@ -1,31 +1,45 @@
 package com.fintern.ourbudgeting.ui.assetmanagement.assetedition
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.fintern.ourbudgeting.R
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.fintern.ourbudgeting.ui.assetmanagement.assetedition.component.AssetEditBody
 import com.fintern.ourbudgeting.ui.assetmanagement.assetedition.component.AssetEditTopAppBar
-import com.fintern.ourbudgeting.ui.assetmanagement.common.component.AssetTitle
-import com.fintern.ourbudgeting.ui.assetmanagement.common.component.AssetTopSection
+import com.fintern.ourbudgeting.ui.assetmanagement.common.component.AssetTypeListTitle
 import com.fintern.ourbudgeting.ui.theme.OurBudgetingTheme
 
 @Composable
 fun AssetEditScreen(
     modifier: Modifier = Modifier,
-    asset: Long,
-    debt: Long
+    onNavigateBack: () -> Unit = {},
+    viewModel: AssetEditViewModel = hiltViewModel(),
+    householdId: String,
 ) {
+
+    val uiState by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(householdId) {
+        viewModel.observeAssetTypes(householdId)
+    }
     Scaffold(
         modifier = modifier.background(Color.White),
-        topBar = { AssetEditTopAppBar() }
+        topBar = { AssetEditTopAppBar(onActionClick = onNavigateBack) }
     ) { paddingValue ->
         Column(
             modifier = Modifier
@@ -33,11 +47,44 @@ fun AssetEditScreen(
                 .background(Color.White)
                 .padding(paddingValue)
         ) {
-            AssetTopSection(asset = asset, debt = debt)
-            AssetTitle(name = stringResource(R.string.cash), amount = 1000)
-            AssetEditBody(name = stringResource(R.string.cash), amount = 1000)
-            AssetTitle(name = stringResource(R.string.card), amount = 2000)
-            AssetEditBody(name = stringResource(R.string.card), amount = 1000)
+            AssetTypeListTitle()
+            if (uiState.isLoading) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            } else {
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    items(
+                        items = uiState.assetTypes,
+                        key = { it }
+                    ) { assetType ->
+                        AssetEditBody(
+                            name = assetType,
+                            editText = if (uiState.editingAsset == assetType) uiState.editText else "",
+                            isEditing = uiState.editingAsset == assetType,
+                            isUpdating = uiState.isUpdating && uiState.editingAsset == assetType,
+                            onEditingChange = { isEditing ->
+                                if (isEditing) {
+                                    viewModel.startEditing(assetType)
+                                } else {
+                                    viewModel.cancelEditing()
+                                }
+                            },
+                            onEditTextChange = { text ->
+                                viewModel.updateEditText(text)
+                            },
+                            onEditComplete = { oldName, newName ->
+                                viewModel.updateAssetType(householdId, oldName, newName)
+                            }
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -46,6 +93,6 @@ fun AssetEditScreen(
 @Composable
 fun ModifyAssetManagementScreenPreview() {
     OurBudgetingTheme {
-        AssetEditScreen(asset = 300, debt = 2000)
+        AssetEditScreen(householdId = "")
     }
 }
