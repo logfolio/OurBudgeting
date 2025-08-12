3f5e2c7ed66bd8f5ae3249adf61adc11e55d6ee6
Meezzi <alswl-3016@naver.com>
2025-08-12 15:58:45 +0900

    feat: 이미지 업로드 실패 타입 추가

--- app/src/main/java/com/fintern/ourbudgeting/ui/common/model/FirebaseError.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/common/model/FirebaseError.kt
@@ -6,5 +6,6 @@ import com.fintern.ourbudgeting.R
 sealed class FirebaseError(@StringRes val messageResId: Int) {
     object NetworkError : FirebaseError(R.string.error_network)
     object FirestoreError : FirebaseError(R.string.error_firestore)
+    object ImageUploadError : FirebaseError(R.string.error_image_upload)
     object UnknownError : FirebaseError(R.string.error_unknown)
 }
