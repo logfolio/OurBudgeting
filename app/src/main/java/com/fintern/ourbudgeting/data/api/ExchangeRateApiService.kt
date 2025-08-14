24f5a256f9211cbac4f9486bb01eaf9b8484040e
0508gyhun <0508gyhun@naver.com>
2025-08-14 17:11:33 +0900

    feat: 환율 apiService 정의
    Co-authored-by: Meezzi <alswl-3016@naver.com>

---
+++ app/src/main/java/com/fintern/ourbudgeting/data/api/ExchangeRateApiService.kt
@@ -0,0 +1,14 @@
+package com.fintern.ourbudgeting.data.api
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ExchangeRateApiService {
+    @GET("site/program/financial/exchangeJSON")
+    suspend fun getExchangeRates(
+        @Query("authkey") authKey: String,
+        @Query("searchdate") searchDate: String? = null,
+        @Query("data") data: String = "AP01"
+    ): Response<List<ExchangeRate>>
+}
