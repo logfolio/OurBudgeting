4d15b3a1b10d63422cb9d81c6890090059f2b146
Meezzi <alswl-3016@naver.com>
2025-08-15 17:04:53 +0900

    feat: 기준 환율(중간값) deal_bas_r 추가

--- app/src/main/java/com/fintern/ourbudgeting/data/model/ExchangeRate.kt
+++ app/src/main/java/com/fintern/ourbudgeting/data/model/ExchangeRate.kt
@@ -10,4 +10,6 @@ data class ExchangeRate(
     val curUnit: String,         // 통화코드
     @SerialName("cur_nm")
     val curNm: String,           // 국가/통화명
+    @SerialName("deal_bas_r")
+    val dealBasR: String
 )
