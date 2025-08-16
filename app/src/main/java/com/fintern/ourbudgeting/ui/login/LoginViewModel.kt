1b29bccf319dcb065de3f810e6b5ffc7e42f48b2
mandol96 <chocan0202@gmail.com>
2025-08-16 23:38:06 +0900

    refactor: import 정리

--- app/src/main/java/com/fintern/ourbudgeting/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/login/LoginViewModel.kt
@@ -1,6 +1,5 @@
 package com.fintern.ourbudgeting.ui.login
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fintern.ourbudgeting.data.auth.GoogleSignInClient
