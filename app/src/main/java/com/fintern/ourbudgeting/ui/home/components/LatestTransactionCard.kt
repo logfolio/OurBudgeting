3344721742cf4c3a0167777c7527571401169fad
Meezzi <alswl-3016@naver.com>
2025-08-20 16:57:33 +0900

    refactor: 카드 UI 정렬

--- app/src/main/java/com/fintern/ourbudgeting/ui/home/components/LatestTransactionCard.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/home/components/LatestTransactionCard.kt
@@ -1,6 +1,7 @@
 package com.fintern.ourbudgeting.ui.home.components
 
 import android.net.Uri
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -41,7 +43,9 @@ fun LatestTransactionCard(
         shape = RoundedCornerShape(8.dp)
     ) {
         Row(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Column {
                 Text(
