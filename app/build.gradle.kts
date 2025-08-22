b3152cfd88bc4db50ce6ea646cfbff0755ca35df
0508gyhun <0508gyhun@naver.com>
2025-08-22 14:58:41 +0900

    feat: release 버전 설정
    Co-authored-by: mandol96 <chocan0202@gmail.com>
    Co-authored-by: Meezzi <alswl-3016@naver.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.fintern.ourbudgeting"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 70
+        versionName = "0.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
