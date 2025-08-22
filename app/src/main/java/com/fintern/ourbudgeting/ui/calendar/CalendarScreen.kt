a054dbe99df1fce904aed267252758b2c7756d9d
mandol96 <chocan0202@gmail.com>
2025-08-22 11:37:36 +0900

    feat: 상단 마진 설정

--- app/src/main/java/com/fintern/ourbudgeting/ui/calendar/CalendarScreen.kt
+++ app/src/main/java/com/fintern/ourbudgeting/ui/calendar/CalendarScreen.kt
@@ -6,12 +6,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -158,6 +158,8 @@ fun CalendarScreen(
             .fillMaxSize()
             .background(Color.White)
     ) {
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+
         item {
             CalendarAccountAndUser(
                 selectedAccount = selectedAccount,
