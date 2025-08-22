1241f0ba52be885c9934a000931db6998f783e66
Meezzi <alswl-3016@naver.com>
2025-08-22 12:25:26 +0900

    chore: kotlinOptions에서 compilerOptions로 마이그레이션
    Co-authored-by: mandol96 <chocan0202@gmail.com>
    Co-authored-by: 0508gyhun <0508gyhun@naver.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.util.Properties
 
 plugins {
@@ -50,8 +51,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
     }
     buildFeatures {
         compose = true
